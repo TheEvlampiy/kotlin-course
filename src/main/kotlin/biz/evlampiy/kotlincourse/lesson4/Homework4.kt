5469b5549a4bdf8050fe1235ceebad5b5fbfe148
Vladimir <vovchik.94@gmail.com>
2024-09-14 23:55:04 +0500

    Lesson 4

---
+++ src/main/kotlin/biz/evlampiy/kotlincourse/lesson4/Homework4.kt
@@ -0,0 +1,93 @@
+package biz.evlampiy.biz.evlampiy.kotlincourse.lesson4
+
+val fortytwoInt: Int = 42
+
+val fortytwoShort: Short = 42
+
+val fourtytwoByte: Byte = 42
+
+val bignumba: Long = 98765432123456789L
+
+val twothreefourfive: Float = 23.45f
+
+val fromzerotonine: Double = 0.123456789
+
+val KotlinJava: String = "Kotlin & Java"
+
+val firstBoolean: Boolean = false
+
+val cChar: Char = 'c'
+
+val fivehundredInt: Int = 500
+
+val fivehundredShort: Short = 500
+
+val anotherbignumba: Long = 4294967296L
+
+val eighteen: Float = 18.0f
+
+val smol: Double = -0.001
+
+val openai: String = "OpenAI"
+
+//“true” - Обман, чтобы набрать «классы»
+
+val nine: Char = '9'
+
+val goodphonegameInt: Int = 2048
+
+val goodphonegameShort: Short = 2048
+
+val tenbillion: Long = 10000000000L
+
+val fivesevenfive: Float = 5.75f
+
+val onefouronefour: Double = 1.414
+
+val ai: String = "Artificial Intelligence"
+
+val secondBoolean: Boolean = false
+
+val at: Char = '@'
+
+val byteInt: Int = 1024
+
+val byteShort: Short = 1024
+
+val bignumbaAgain: Long = 1234567890123L
+
+val imTiredOfComingUpWithNames: Float = 10.01f
+
+val a: Double = -273.15
+
+val spaceX: String = "SpaceX"
+
+val thirdBoolean: Boolean = true
+
+//“🤯” - 🤯
+
+//‘65535’ - Got me this time!
+
+val b: Long = 72057594037927935L
+
+val c: Float = 2.71828f
+
+val d: Double = 101.0101
+
+val quantumComputer: String = "Quantum Computing"
+
+val fourthBoolean: Boolean = false
+
+val x: Char = 'x'
+
+val almostPiInt: Int = 314
+
+val almostPiShort: Short = 314
+
+val e: Long = 123456789123456789L
+
+val f: Float = 6.626f
+
+val g: Double = 0.007
+
+val theLastOne: String = "Android Studio"
